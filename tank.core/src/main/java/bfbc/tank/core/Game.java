a0aa72a03356aab96c34ff9ac766d3d1dbe93fc7
Vasily <just.one.man@yandex.ru>
2016-01-19 17:54:34 +0300

    Missile speed now should depend on tank speed

--- tank.core/src/main/java/bfbc/tank/core/Game.java
+++ tank.core/src/main/java/bfbc/tank/core/Game.java
@@ -203,7 +203,7 @@ public class Game extends Thread implements MissileCrashListener {
 	synchronized Missile createMissile(Player p, double posX, double posY, double angle, double velocity) {
 		int index = findPlayerId(p);
 		if (missiles.get(index).isEmpty()) {
-			Missile newMissile = new Missile(this, this, collider, findPlayerId(p), posX, posY, angle, velocity);
+			Missile newMissile = new Missile(this, this, collider, index, posX, posY, angle, velocity);
 			missiles.get(index).add(newMissile);
 			collider.addAgent(newMissile);
 			return newMissile;
